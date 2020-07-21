f516d78ac62adab99ae70a9f8e0e89eb8aaf290e
abdullatifmouhamadi <abdullatif.mouhamadi@gmail.com>
2020-07-21 16:04:18 +0300

    updating compiler option

--- core/latex.py
+++ core/latex.py
@@ -14,8 +14,9 @@ class Latex:
     # 
     def compile(self, path, src):
         try:
-
-            r = self.bash.run('cd {} && latexmk -synctex=1 -interaction=nonstopmode -file-line-error -pdf {}.tex -outdir="."'.format(path, src))
+            #compiler = "-pdf" # -pdflatex
+            compiler = "-xelatex" # -xelatex
+            r = self.bash.run('cd {} && latexmk {} -synctex=1 -interaction=nonstopmode -file-line-error {}.tex -outdir="."'.format(path, compiler, src))
             #r = self.bash.run('cd {} && latexmk -synctex=1 -interaction=nonstopmode -file-line-error -pdf -g {}.tex -outdir="."'.format(path, src)) force 'g' parma
             #r = self.bash.run('cd {} && bibtex {}'.format(path, src))
             #r = self.bash.run('cd {} && pdflatex -synctex=1 -interaction=nonstopmode -file-line-error {}'.format(path, src))
