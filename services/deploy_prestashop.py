f733ade5bf741e328b9e2c92d6ddb7960ba8a836
abdullatifmouhamadi <abdullatif.mouhamadi@gmail.com>
2020-08-16 12:05:42 +0200

    updating deploy_prestashop

--- services/deploy_prestashop.py
+++ services/deploy_prestashop.py
@@ -11,6 +11,31 @@ class Prestashop(Deployable):
                             app_name = app_name,
                             params = params)
         
+    def update_nginx_template(self):
+        MYPORT = '80' #self.port
+        MYHOST = self.stage['host']
+        NGINX_FILENAME = self.nginx_filename
+        WORKINGDIR = self.appDir + '/src/prestashop/'
+
+        self.rc.sed(self.nginx_available, 'MYPORT', MYPORT)
+        self.rc.sed(self.nginx_available, 'MYHOST', MYHOST)
+        self.rc.sed(self.nginx_available, 'NGINX_FILENAME', NGINX_FILENAME)
+        self.rc.sed(self.nginx_available, 'WORKINGDIR', WORKINGDIR.replace('/', r'\/'))
+
+        # link
+        self.rc.linksoft(self.nginx_available, self.nginx_enabled)
+
+        # reload nginx
+        self.rc.reloadnginx()
+
+    def config_nginx_template(self):
+        print("\n\n==> config_nginx_template\n\n")
+        self.rc.copy(src    = self.appDir + '/deploy/archlinux/template-nginx',
+                     target = self.nginx_available)
+
+        self.update_nginx_template()
+
+
 
     def install_prestashop(self):
         SCRIPT_PATH = self.appDir + '/script/'
@@ -27,7 +52,12 @@ class Prestashop(Deployable):
         self.setup_git_env()
 
 
+        #
         self.install_prestashop()
+
+
+        # nginx
+        self.config_nginx_template()
 
 
 
