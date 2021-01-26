fc052a3961f828ea2ddaf3c8ddb4ac55e6ca6804
abdullatifmouhamadi <abdullatif.mouhamadi@gmail.com>
2021-01-26 10:43:48 +0100

    update pack

--- templates/posbox/script/install_packages.sh
+++ templates/posbox/script/install_packages.sh
@@ -8,7 +8,7 @@
 pacman -S unzip postgresql python-lxml\
  python-gevent libsass cups dbus ipp-usb\
   python-gobject gobject-introspection\
-  bluez bluez-utils net-tools \
+  bluez bluez-utils net-tools wireless_tools\
   --noconfirm --needed
  
 
