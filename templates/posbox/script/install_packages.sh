d8293d31d953b47f6be65b76cb39c442fd8e3454
abdullatifmouhamadi <abdullatif.mouhamadi@gmail.com>
2021-01-26 13:50:14 +0100

    adding

--- templates/posbox/script/install_packages.sh
+++ templates/posbox/script/install_packages.sh
@@ -9,6 +9,7 @@ pacman -S unzip postgresql python-lxml\
  python-gevent libsass cups dbus ipp-usb\
   python-gobject gobject-introspection\
   bluez bluez-utils net-tools wireless_tools\
+  xdotool\
   --noconfirm --needed
  
 
